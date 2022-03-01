700ba03a99748e7ba81e08ce88dc52be586a807a
asif <asif@gmail.com>
2022-03-01 23:44:27 +0600

    change

--- script.js
+++ script.js
@@ -16,7 +16,10 @@
     
     const displayAll=(country)=>{
     const container=document.getElementById('country');
-    container.innerHTML=`
+    container.innerHTML='';
+    
+    const div=document.createElement('div');
+    div.innerHTML=`
                             <h1>Country name: ${country.name.common}</h1> 
                             <h1>Capital: ${country.capital}</h1>
                             <h1>Population: ${country.population}</h1>
@@ -29,10 +32,36 @@
                             </div>
                             <br>
                             <div id='button'>
-                            <button onclick="showMore()" id='show-more-btn'> Show more details </button>
+                            <button onclick=details('${country.name.common}') id='show-more-btn'> Show more details </button>
                             <button  onclick="showLess()" id='show-less-btn' >Show less</button>
                             </div>
     `
+    container.appendChild(div)
+    }
+
+    const details=(param)=>{
+       const url=`https://restcountries.com/v3.1/name/${param}`
+       fetch(url)
+       .then(response=>response.json())
+       .then(data=>displayDetails(data))
+    }
+
+
+    const displayDetails=(info)=>{
+        const container=document.getElementById('more-details');
+    
+    const div=document.createElement('div');
+    div.innerHTML=`
+                            <h1>Country name: ${info.name.common}</h1> 
+                            <h1>Capital: ${info.capital}</h1>
+                            <h1>Population: ${info.population}</h1>
+                            <h1>Flag</h1>
+                            <img src=${info.flags.png}>
+                                <h1>Official name:${info.name.official}</h1>
+                                <h1>Independent status:${info.independent}</h1>
+                                <h1>Languages:${info.languages[1]}</h1>
+                                `
+     container.appendChild(div)
     }
     
     
